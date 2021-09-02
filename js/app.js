543c526476f009d9d8f42ffbf4e7d01d9b96f884
mdkamrul2day <mdkamrul2day@gmail.com>
2021-09-02 12:42:36 +0600

    all complited

--- js/app.js
+++ js/app.js
@@ -16,51 +16,44 @@ searchButton.addEventListener('click', function(){
     const url = `https://openlibrary.org/search.json?q=${search}`;
     fetch(url)
     .then((res) => res.json())
-    .then((data) => {
-        console.log(data.numFound);
-        if (data.numFound === 0){
-            errorHandler.innerText = "NO result fount"
-        }
-        else{
-                    // console.log(data.docs)
-        data.docs.forEach((item) => {
-            // console.log(item);
-
-            const div = document.createElement("div");
-            div.classList.add("col-md-3");
-            div.innerHTML =`
-                    <div class="rounded overflow-hidden border">
-                    <img src="https://restcountries.eu/data/umi.svg" class="img-fluid"  alt="" />
-                    </div>
-
-                    <div class="
-                      py-2
-                      d-flex
-                      justify-content-between
-                      align-items-center
-                      d-md-block
-                      text-md-center
-                    ">
-                        <h3>${item.title}</h3>
-                        <h5>Author Name:${item.author_name}</h5>
-                        <p>First publish:${item.first_publish_year}</p>
-                    </div>
-                `;
-            resultContainer.appendChild(div);
-            //Clear Input Value
-            searchInput.value = "";
-        })
-        }
-
-    });
+    .then((data) =>displayData(data));
 });
 
+const displayData = (books) => {
+    if (books.numFound === 0){
+        errorHandler.innerText = "NO result fount"
+    }
+    else{
+        // Total Found
+        errorHandler.innerText = `Total found: ${(books.docs).length}`
+        // Loop
+        books.docs.forEach((book) => {
+        console.log(book);
 
+        const div = document.createElement("div");
+        div.classList.add("col-md-3");
+        div.innerHTML =`
+                <div class="rounded overflow-hidden border">
+                <img src='https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg' class="img-fluid  mx-auto d-block"  alt="" />
+                </div>
 
+                <div class="
+                  py-2
+                  d-flex
+                  justify-content-between
+                  align-books-center
+                  d-md-block
+                  text-md-center
+                ">
+                    <h3>${book.title}</h3>
+                    <h5>Author Name:${book.author_name}</h5>
+                    <p>First publish:${book.first_publish_year}</p>
+                </div>
+            `;
+        resultContainer.appendChild(div);
+        //Clear Input Value
+        searchInput.value = "";
+    })
+    };
 
-
-/* const url = http://openlibrary.org/search.json?q=${searchText};
-        fetch(url)
-            .then(res => res.json())
-            .then(data => displaySearchResult(data.docs));
- */
+};
